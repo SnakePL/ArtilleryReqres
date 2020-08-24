c454d62d39dac897bb65121a1236fee00e9b3bb7
MateuszP <przekwasmateusz@gmail.com>
2020-08-24 10:56:07 +0200

    adding a comment to the .js file

--- getUsersIDs.js
+++ getUsersIDs.js
@@ -8,3 +8,11 @@ function saveIDsFromResponse(requestParams, response, context, ee, next) {
 module.exports = {
 	saveIDsFromResponse: saveIDsFromResponse
 };
+/*
+Metoda getIds - mapuje nam na idki z jakiekolwiek obiektu
+Metoda saveIDs…  bierze response.body parsuje nam do Json, zeby to nie było string, 
+i przpisuje to tej pustej arrayki arrayOfUsers
+cwiczenia:
+sprawdzic saveIDs w console.log(context),
+console.log(context.vars) itd zeby zobaczyc jak to dziala
+*/
